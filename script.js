dc7438569622b5fe291e69b3a21b09fc9c1c95f8
N10T <andy.tisba@gmail.com>
2021-02-11 18:18:24 +0100

    setup

---
+++ script.js
@@ -0,0 +1,15 @@
+//find all element with "tab" class and have it as a NodeList
+
+//find all element with "Page" class and have it as a NodeList
+
+//create a function named render
+//taking 1 argument "event"
+    //in render function
+// in render find the element of the event
+// find the value of the custom attribute "data-section" of the previous element
+// add the class hidden to all "tab" elements 
+// find the div with the id of the data-section value previously found
+// remove the class hidden to all "tab" elements 
+
+// add the event listner to all the the tabs
+// add the event handler "render" function
